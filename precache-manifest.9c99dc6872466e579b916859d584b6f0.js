001f8b8c1db5b096e0a7f4f4c90b96467584d5ac
AbdallahWario <abdallwario96@gmail.com>
2020-03-19 23:42:44 +0300

    Updates

---
+++ precache-manifest.9c99dc6872466e579b916859d584b6f0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b95762f8091fbfcb5f3e5a5c366d2fe",
+    "url": "/forex/index.html"
+  },
+  {
+    "revision": "0e9c7b1b86bc061962f4",
+    "url": "/forex/static/css/2.6365e538.chunk.css"
+  },
+  {
+    "revision": "303e4f94aa74b6fa370d",
+    "url": "/forex/static/css/main.507bf6f5.chunk.css"
+  },
+  {
+    "revision": "0e9c7b1b86bc061962f4",
+    "url": "/forex/static/js/2.7a1cb6ee.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/forex/static/js/2.7a1cb6ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "303e4f94aa74b6fa370d",
+    "url": "/forex/static/js/main.baf0cd45.chunk.js"
+  },
+  {
+    "revision": "24460636a6289b56ecb2",
+    "url": "/forex/static/js/runtime-main.746242fa.js"
+  },
+  {
+    "revision": "f1fdf59f696f9b789da5e1c8b28d3ebe",
+    "url": "/forex/static/media/arrow.f1fdf59f.svg"
+  },
+  {
+    "revision": "cd071a540d95b836ede9122fcca287a9",
+    "url": "/forex/static/media/bg.cd071a54.jpeg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/forex/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/forex/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/forex/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/forex/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/forex/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
