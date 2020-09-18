f9d8dcaa87d6ad55e3c4bc50c1e5a79586553ee4
wario_abdallah <33093727+AbdallahWario@users.noreply.github.com>
2020-09-18 15:35:08 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,10 +12,13 @@ export default function Home() {
     </div>
  
   <h1 className="homename">Get started with Forex Today</h1>
-  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis doloribus,
-  dignissimos in nisi temporibus quidem impedit eaque saepe, veritatis tenetur
-  magnam quas suscipit velit eveniet mollitia vel magni sit, ipsam iste obcaecati 
-  consequatur! Dolore laboriosam reiciendis amet eaque excepturi sint?</p>
+  <p>
+        Fx prime traders getting started
+
+Getting started with copying our trades is learning first the Basics of forex trading and the industry. There are different ways that you can participate in trading currencies in the global forex market, the first way is copy trading which closely resembles the mirror trading and social trading.By copying our trades,you will connect your margined forex account to our copy trading account and our Proffesionally generated buy and sell signals will be automatically be copied to your forex margined account, while you do nothing but managing the risks and portion of money that you are willing to risk for copying our trades. We use a combination of different intraday trading strategies in a perfect blend to generate our buy and sell signals. 
+Our proven and tested forex trading strategies have a win rate of 85% and a maximum Draw Down percentage of 13% a rate suitable for moderate risk appetite investors, that is sought by many copy trading gurus.It is important to note that we don't apply the risky martingale system in our trading strategy. 
+The most common and traditional way of trading  is trading manually, either through fundamental analysis or the chart analysis.Kindly we don't offer manual trading where forex traders Master the use of different forex trading tools and indicators to manually buy or sell currencies.Choose your  diligent forex Broker with low spreads and fee and one who is under a regulatory jurisdiction. Once you have chosen your broker open a demo account first to familiarize yourself with the risk management settings of your copy trading account. After mastering your risk management you can automatically copy our trades in 24 hours, even as you sleep your trades will be executed by our team.Let your forex account be a passive income investment,a money making machine!
+  </p>
 
  </div>
 
